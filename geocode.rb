db1afa87fd12f4ee5731547fa0f0440021fd0c98
Mark Silverberg <mark@marksilver.net>
2013-03-03 08:03:42 -0500

    code clean and adding output about geocoding process

--- geocode.rb
+++ geocode.rb
@@ -6,15 +6,17 @@ Bundler.require
 # configure and set up MongoDB connection
 mongo_client = Mongo::MongoClient.new("localhost", 27017)
 onc_db = mongo_client.db("onc")
-provider_col = onc_db.collection("paid_by_ehr_program")
+providers_col = onc_db.collection("paid_by_ehr_program")
 
-puts "Number of providers in collection: #{provider_col.count}"
+puts "Number of providers in collection: #{providers_col.count}"
 
-# find providers without geo info and populate using Cloudmate
-provider_col.find.limit(10000).each do |p|
+providers_without_geo = providers_col.find("geo" => nil)
+puts "Number of providers in collection w/o geolocation: #{providers_without_geo.count}"
+
+providers_without_geo.each do |p|
   address_to_lookup = "#{p["PROVIDER  ADDRESS"]}, #{p["PROVIDER CITY"]}, #{p["PROVIDER STATE"]} #{p["PROVIDER ZIP 5 CD"]}"
   puts address_to_lookup
-  geo_results = JSON.parse(RestClient.get("http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address="+URI.encode(address_to_lookup)))
+  geo_results = JSON.parse(RestClient.get("http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address="+URI.encode(address_to_lookup.gsub(/[^a-zA-Z\d\s,]/,""))))
   if geo_results["status"] == "OK"
     geo_data = {
       "provider" => "DSTK",
@@ -22,7 +24,7 @@ provider_col.find.limit(10000).each do |p|
       "data" => geo_results["results"].first
     }
     p["geo"] = geo_data
-    provider_col.update({"_id" => p["_id"]}, p)
+    providers_col.update({"_id" => p["_id"]}, p)
     puts "Done."
   end
 end

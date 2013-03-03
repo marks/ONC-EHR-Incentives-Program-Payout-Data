e504ac76e706a9e278d08f3c96c0b47b42a43fbd
Mark Silverberg <mark@marksilver.net>
2013-03-02 23:53:36 -0500

    more code to play nice with cloudmade limitations of characters in the address being geocoded

--- geocode.rb
+++ geocode.rb
@@ -15,16 +15,21 @@ cloudmade = CloudMade::Client.from_parameters('ed5940a5e0724173aba9da0d77368912'
 puts "Number of providers in collection: #{provider_col.count}"
 
 # find providers without geo info and populate using Cloudmate
-provider_col.find("geo" => nil).limit(1).skip(1).each do |p|
+provider_col.find("geo" => nil).limit(10000).skip(1).each do |p|
   address_to_lookup = "#{p["PROVIDER  ADDRESS"]}, #{p["PROVIDER CITY"]}, #{p["PROVIDER STATE"]} #{p["PROVIDER ZIP 5 CD"]}"
-  geo_result = cloudmade.geocoding.find(address_to_lookup).results.first
-  geo_data = {
-    "provider" => "CloudMade",
-    "updated_at" => Time.now,
-    "properties" => geo_result.properties,
-    "centroid" => [geo_result.centroid.lat, geo_result.centroid.lon]
-  }
-  p["geo"] = geo_data
-  provider_col.update({"_id" => p["_id"]}, p)
-  puts "Updated"
+  puts address_to_lookup
+  begin # sometimes cloudmade doesnt respond nicely
+    geo_result = cloudmade.geocoding.find(address_to_lookup.gsub(/[^a-zA-Z\d\s,]/,"")).results.first # cloudmade cant handle special characters in the query and we're only interested in the first result.
+    geo_data = {
+      "provider" => "CloudMade",
+      "updated_at" => Time.now,
+      "properties" => geo_result.properties,
+      "centroid" => [geo_result.centroid.lat, geo_result.centroid.lon]
+    }
+    p["geo"] = geo_data
+    provider_col.update({"_id" => p["_id"]}, p)
+    puts "Updated"
+  rescue
+    # just move on for now, we'll geocode this provider later
+  end
 end

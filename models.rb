12181a3508aabae6a38f667227d62c39ab5e50ad
Mark Silverberg <mark@marksilver.net>
2013-03-03 22:02:30 -0500

    adding indices to models.rb

--- models.rb
+++ models.rb
@@ -2,10 +2,14 @@ Mongoid.load!("config/mongoid.yml")
 
 class Hospital
   include Mongoid::Document
+  index({ "PROVIDER STATE" => 1})
+  index({ "geo.data.geometry.location" => "2d"})
   store_in collection: "ProvidersPaidByEHRProgram_Dec2012_HOSP_FINAL"
 end
 
 class Provider
   include Mongoid::Document
+  index({ "PROVIDER STATE" => 1})
+  index({ "geo.data.geometry.location" => "2d"})
   store_in collection: "ProvidersPaidByEHRProgram_Dec2012_EP_FINAL"
 end

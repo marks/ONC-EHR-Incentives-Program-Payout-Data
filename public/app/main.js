ae6e8442f59a5639fe1830921905992036944be9
Mark Silverberg <mark@marksilver.net>
2013-03-06 15:02:12 -0500

    use cloudfront to cache provider queries too.

--- public/app/main.js
+++ public/app/main.js
@@ -41,7 +41,7 @@ function onFeatureClick(e){
 function constructComparisonTable(){
   $("#comparison_tables").html("") // clear the comparison table div
   $.each(features_clicked, function(n,feature){
-    provider_url = "/db/onc/ProvidersPaidByEHRProgram_Dec2012_HOSP_FINAL/find_by_ccn/"+feature.properties["PROVIDER CCN"]+".json"
+    provider_url = "http://cf.hitech.socialhealthinsights.com/db/onc/ProvidersPaidByEHRProgram_Dec2012_HOSP_FINAL/find_by_ccn/"+feature.properties["PROVIDER CCN"]+".json"
     $.getJSON(provider_url, function(data){
       if(data == null){
         // do nothing

876cc8e11606f9df069089f0fbf6986e00809d24
Mark Silverberg <mark@socialhealthinsights.com>
2013-12-12 13:06:36 -0500

    #32 commas

--- models.rb
+++ models.rb
@@ -6,7 +6,7 @@ module ModelHelpers
     incentives = {}
     (2011..2013).each do |year|
       incentives["year_#{year}"] = (self["PROGRAM YEAR #{year}"].to_s.upcase == "TRUE")
-      incentives["year_#{year}_amt"] = (self["PROGRAM YEAR #{year} CALC PAYMENT"].to_s.gsub(",","").to_f)
+      incentives["year_#{year}_amt"] = (self["PROGRAM YEAR #{year} CALC PAYMENT"])
     end
     return incentives
   end
